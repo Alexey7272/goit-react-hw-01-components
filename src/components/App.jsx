b5b97a9f8ec5ad44d93656775b858aa7a0d9a5fb
Alexey7272 <106250222+Alexey7272@users.noreply.github.com>
2022-12-31 10:56:11 +0200

    do 2 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,31 @@
-import userInfo from 'user.json'
+// TASK 1
+import userInfo from './InfoUser/user.json'
 import {InfoUser} from 'components/InfoUser/InfoUser'
+
+// TASK 2
+import {Data} from 'components/Data/Data.jsx'
+
+// TASK 3
+import FriendsListItem from './FriendsList/friends.json'
+import {FriendsList} from './FriendsList/FriendsList'
 
 export const App = () => {
   return (
     <div>
       <InfoUser
-      // key={userInfo.username}
       username={userInfo.username}
       tag={userInfo.tag}
       location={userInfo.location}
-      avatar={userInfo.avatar} 
+      avatar={userInfo.avatar}
+      followers={userInfo.stats.followers}
+      views={userInfo.stats.views}
+      likes={userInfo.stats.likes}
+      />
+      <Data
+      title={'Upload stats'}
+      />
+      <FriendsList
+      friends={FriendsListItem}
       />
     </div>
   );
---
+++ src/components/Data/Data.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types'
+import css from './Data.module.css'
+import getRandomHexColor from 'utils/RandomColor.js'
+import Datacleaner from 'utils/datacleaner';
+import stat from './data.json'
+
+const cleanData = Datacleaner(stat)
+
+export const Data = ({ title }) => {
+    return (
+        <section className={css.statistics}>
+        {title && <h2 className={css.title}>{title}</h2>}
+
+            <ul className={css.stat_list}>
+                {cleanData.map(stat => 
+                    <li key={stat.id} className={css.item} style={{backgroundColor: getRandomHexColor() }}>
+                       <span className={css.label}>{stat.label}</span>
+                       <span className={css.percentage}>{stat.percentage}</span>
+                    </li>
+                )}
+            </ul>
+        </section>
+    );
+};
+
+
+Data.propTypes = {
+    title: PropTypes.string
+};
